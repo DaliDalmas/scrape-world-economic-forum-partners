d0e120e03e2f305ebc93712e315f94a0d91c6c69
Dalmas.Otieno <tutor.dalmas.otieno@gmail.com>
2023-07-27 00:44:52 +0300

    done

--- crawl.py
+++ crawl.py
@@ -5,30 +5,45 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
 
-links_df = pd.read_csv('orgs_with_links.csv')
-scraped_data = pd.read_csv('organizations.csv')
-links_df = links_df[links_df['link'].apply(lambda l: str(l) not in list(scraped_data['link']))]
+def run():
+    links_df = pd.read_csv('orgs_with_links.csv')
+    scraped_data = pd.read_csv('organizations.csv')
+    links_df = links_df[links_df['link'].apply(lambda l: str(l) not in list(scraped_data['link']))]
 
-for idx, partner_type, link in links_df.to_records():
-    print(idx, link)
-    driver_obj = MakeDriver(link)
-    driver_obj.create_driver()
+    for idx, partner_type, link in links_df.to_records():
+        print(idx, link)
+        driver_obj = MakeDriver(link)
+        driver_obj.create_driver()
 
-    org_name_xpath = '//h1[@class="organization__name"]'
-    WebDriverWait(driver_obj.website_driver, 5).until(
-        EC.presence_of_element_located((By.XPATH, org_name_xpath))
-    )
+        org_name_xpath = '//h1[@class="organization__name"]'
+        WebDriverWait(driver_obj.website_driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, org_name_xpath))
+        )
 
-    organization_name = driver_obj.website_driver.find_element(By.XPATH, org_name_xpath).text
+        organization_name = driver_obj.website_driver.find_element(By.XPATH, org_name_xpath).text
 
-    org_desc_xpath = '//div[@class="organization__description"]/p'
-    organization_description = driver_obj.website_driver.find_element(By.XPATH, org_desc_xpath).text.replace('\n', '').replace(',', ' ').replace("'", "")
+        org_desc_xpath = '//div[@class="organization__description"]/p'
+        organization_description = driver_obj.website_driver.find_element(By.XPATH, org_desc_xpath).text.replace('\n', '').replace(',', ' ').replace("'", "")
 
-    org_hq_xpath = '//dl[@class="column organization__profile__stats"]/dd'
-    organization_headquarters = driver_obj.website_driver.find_element(By.XPATH, org_hq_xpath).text
+        org_hq_xpath = '//dl[@class="column organization__profile__stats"]/dd'
+        organization_headquarters = driver_obj.website_driver.find_element(By.XPATH, org_hq_xpath).text
 
-    org_site_xpath = '//a[@class="organization__website--dark"]'
-    organization_site = driver_obj.website_driver.find_element(By.XPATH, org_site_xpath).get_attribute('href')
+        org_site_xpath = '//a[@class="organization__website--dark"]'
+        organization_site = driver_obj.website_driver.find_element(By.XPATH, org_site_xpath).get_attribute('href')
 
-    with open('organizations.csv', 'a') as f:
-        f.write(f"""{organization_name},{organization_headquarters},{organization_site},{organization_description},{link},{partner_type}\n""")
+        with open('organizations.csv', 'a') as f:
+            f.write(f"""{organization_name},{organization_headquarters},{organization_site},{organization_description},{link},{partner_type}\n""")
+
+if __name__=='__main__':
+    condition = True
+    while condition:
+        try:
+            run()
+        except:
+            print('connection stopped let sleep for a minute')
+            time.sleep(60)
+            try:
+                run()
+            except:
+                print('connection stopped twice let sleep for ten minutes')
+                time.sleep(600)
